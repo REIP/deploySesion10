8aca474c3146170d0c830194bfb0657ac271de69
Pier Herbozo <pier.herbozo@sap.com>
2018-12-15 13:38:37 -0500

    first commit

--- .history/src/app/app.module_20181210192510.ts
+++ .history/src/app/app.module_20181210192510.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
